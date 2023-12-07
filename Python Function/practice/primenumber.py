f6dda265333e148c86ca3176976062feb53c5a57
tejasvmodi <moditejasv@gmail.com>
2023-12-07 12:14:37 +0530

    commit

--- Python Function/practice/primenumber.py
+++ Python Function/practice/primenumber.py
@@ -1,8 +1,9 @@
 c=int(input("enter the number "))
+P=0
 for i in range(2,c):
     if(c%i==0):
         print("it is not prime number")
-        c=c+1
+        P=P+1
     else:
         print("it is  prime number")
         
